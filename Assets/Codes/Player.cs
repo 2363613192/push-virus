29e6869b8c84f47483cffceaaf863cbd3f6aab19
guil21 <2363613192@qq.com>
2022-07-06 23:49:24 +0800

    添加新角色和动画

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -4,16 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 5;
-    private Rigidbody2D mRig;
+    public float runSpeed;
+    public float jumpSpeed;
+    bool IsGround;
 
+    private Rigidbody2D myRb2;
+    private Animator myAnim;
+    private BoxCollider2D myFeet;
+    
     void Start()
     {
-        mRig = GetComponent<Rigidbody2D>();
+        myRb2 = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        myFeet = GetComponent<BoxCollider2D>();
+
+        Application.targetFrameRate = 60;
     }
 
     void Update()
     {
-        mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, mRig.velocity.y);
+        Run();
+        Jump();
+        CheckGrounded();
+        SwitchAnimation();
+    }
+
+    private void Run()
+    {
+        float moveDir = Input.GetAxisRaw("Horizontal");
+        myRb2.velocity = new Vector2(moveDir * runSpeed, myRb2.velocity.y);
+        //bool hasXSpeed =  Mathf.Abs(myRb2.velocity.x) > 0;  // Mathf.Epsilon是一个很小的数
+        //myAnim.SetBool("Run", hasXSpeed);
+        if (moveDir != 0) 
+        {
+            myAnim.SetBool("Run", true);
+            if (moveDir > 0)
+                GetComponent<SpriteRenderer>().flipX = false;
+            else
+                GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+            myAnim.SetBool("Run", false);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGround)
+            {
+                myRb2.velocity = new Vector2(myRb2.velocity.x, jumpSpeed);
+                myAnim.SetBool("Jump", true);
+            }
+        }
+    }
+
+    private void CheckGrounded()
+    {
+        IsGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+
+    private void SwitchAnimation()
+    {
+        myAnim.SetBool("Idle", false);
+        if (myAnim.GetBool("Jump"))
+        {
+            if (myRb2.velocity.y < 0)
+            {
+                myAnim.SetBool("Jump", false);
+                myAnim.SetBool("Fall", true);
+            }
+            else if (IsGround)
+            {
+                myAnim.SetBool("Fall", false);
+                myAnim.SetBool("Idle", true);
+            }
+        }
     }
 }
---
+++ Assets/Codes/Player_2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2: MonoBehaviour
+{
+    private float speed = 5;
+    private Rigidbody2D mRig;
+
+    void Start()
+    {
+        mRig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, mRig.velocity.y);
+    }
+}
